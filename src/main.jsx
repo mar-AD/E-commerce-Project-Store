d69935c97455bc92a3dfb82a67426de0782043de
marra abdallah <abdallah4marra@gmail.com>
2024-05-07 00:16:48 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { CartProvider } from "./Components/cart/cartcontext.jsx";
+import { CartProvider } from "./Components/Cart/Cartcontext.jsx";
 import { AuthProvider } from "./Components/Logincontext/Logincontext.jsx";
 import "./index.css";
 
